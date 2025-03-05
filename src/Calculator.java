14f30cc2ce3812ae89b68980cfc24c29ddda7330
malik <malikrishit0522gmail.com>
2025-03-05 08:21:53 +0530

    Add JButtons and Jpanel

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,6 +2,36 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Calculator {
-    
+public class Calculator implements ActionListener{
+
+    JFrame frame;
+    JTextField textField;
+    JButton[] numberButtons = new JButton[10];
+    JButton[] functionButtons = new JButton[8];
+    JButton addButton, subButton, mulButton, divButton;
+    JButton decButton, equButton, delButton, clrButton;
+    JPanel panel;
+
+    Font myFont = new Font("Fira Mono", Font.BOLD, 30);
+
+    double number1, number2, result;
+    char operator;
+
+    Calculator() {
+        frame = new JFrame("Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420, 580);
+        frame.setLayout(null);
+        
+        
+        frame.setVisible(true);
+    }
+    public static void main(String[] args) {    
+        Calculator calculator = new Calculator();
+    }
+    @Override
+    public void actionPerformed(ActionEvent arg0) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    }
 }
