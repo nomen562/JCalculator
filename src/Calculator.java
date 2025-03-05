d0691d52e6d33bf2a05d3d6e7d71033dd8d2d4f5
malik <malikrishit0522gmail.com>
2025-03-05 19:27:48 +0530

    Add everything to frame

--- src/Calculator.java
+++ src/Calculator.java
@@ -88,7 +88,13 @@ public class Calculator implements ActionListener{
         panel.add(numberButtons[0]);
         panel.add(equButton);
         panel.add(divButton);
-        
+
+		frame.add(panel);
+		frame.add(negButton);
+		frame.add(delButton);
+		frame.add(clrButton);
+		frame.add(textField);
+		frame.setVisible(true);        
     }
 
     public static void main(String[] args) {    
